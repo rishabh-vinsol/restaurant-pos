a03ac9615a83799f63b363ee858b5cdc32cee282
Rishabh Kaushik <rishabh.kaushik@vinsol.com>
2023-05-15 16:22:57 +0530

    Added bootstrap to users listing and did changes according to the comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,21 +5,15 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show
-  end
-
   def new
     @user = User.new
-  end
-
-  def edit
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to user_url(@user), notice: "User was successfully created."
+      redirect_to user_url(@user), notice: 'User was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -27,22 +21,22 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to user_url(@user), notice: "User was successfully updated."
+      redirect_to user_url(@user), notice: 'User was successfully updated.'
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @user.destroy
-
-    redirect_to users_url, notice: "User was successfully destroyed."
+    @user.destroy ?  flash[:notice] = 'User was successfully destroyed.' : flash[:alert] = 'Unable to destroy current user'
+    redirect_to users_url
   end
 
   private
 
   def set_user
     @user = User.find_by(id: params[:id])
+    redirect_to users_path, alert: 'User not found' unless @user
   end
 
   def user_params

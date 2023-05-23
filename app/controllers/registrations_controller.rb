a7c7a2e6a73c9607ec5f1cf6e3da5ff897d8d994
Rishabh Kaushik <rishabh.kaushik@vinsol.com>
2023-05-23 11:16:34 +0530

    Added password confirmation to new user and register forms

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -96,6 +96,6 @@ class RegistrationsController < ApplicationController
   end
 
   private def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,6 +48,6 @@ class UsersController < ApplicationController
   end
 
   private def user_params
-    params.require(:user).permit(:first_name, :last_name, :role, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :role, :email, :password, :password_confirmation)
   end
 end

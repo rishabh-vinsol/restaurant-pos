1d9e11d6d3eb569a4f32c6e5035d38b9603b6d13
Rishabh Kaushik <rishabh.kaushik@vinsol.com>
2023-05-25 19:08:20 +0530

    Fixed nav links and default validation on branch

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,11 +1,13 @@
 # Model class Branch
 class Branch < ApplicationRecord
+  ### VALIDATIONS ###
+
   validates :name, :opening_time, :closing_time, presence: true
   validates :closing_time, comparison: { greater_than: :opening_time }, if: :closing_time
   validate :only_one_default, if: :default
 
   private def only_one_default
-    if Branch.exists?(default: true)
+    if default_was != true && Branch.exists?(default: true)
       errors.add(:default, :present)
     end
   end

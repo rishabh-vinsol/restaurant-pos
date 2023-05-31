8cc8165e571de9d0094bacdb57da0bd16d7310d2
Rishabh Kaushik <rishabh.kaushik@vinsol.com>
2023-05-31 12:34:26 +0530

    Added after commit to set non-veg ingredient meals

--- app/models/ingredient_meal.rb
+++ app/models/ingredient_meal.rb
@@ -13,8 +13,13 @@ class IngredientMeal < ApplicationRecord
   ### CALLBACKS ###
 
   after_commit :set_meal_price
+  after_commit :set_meal_non_veg
 
   private def set_meal_price
     meal.set_price if !persisted? || ingredient_quantity_previously_changed?
+  end
+
+  private def set_meal_non_veg
+    meal.set_non_veg if !persisted? || ingredient_id_previously_changed?
   end
 end

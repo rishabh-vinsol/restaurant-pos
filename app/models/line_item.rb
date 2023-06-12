044b1c17491183ac7ecd972728b745302979b96a
Rishabh Kaushik <rishabh.kaushik@vinsol.com>
2023-06-12 17:37:58 +0530

    Fixed methods ordering in line item model

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -15,18 +15,18 @@ class LineItem < ApplicationRecord
   before_save :set_total, if: proc { |li| li.order.cart? }
   after_commit :update_order_price
 
-  def branch_meal_inventory
-    meal_max_quantity = ingredients_meals.joins(:inventories)
-    .where(inventories: { branch_id: order.branch_id })
-    .minimum('inventories.quantity/ingredients_meals.ingredient_quantity')
-    errors.add(:base, "Cannot add more than #{meal_max_quantity} #{meal.name} to cart") if quantity > meal_max_quantity
-  end
-
   def update_total
     if order.cart?
       set_total
       save
     end
+  end
+
+  private def branch_meal_inventory
+    meal_max_quantity = ingredients_meals.joins(:inventories)
+    .where(inventories: { branch_id: order.branch_id })
+    .minimum('inventories.quantity/ingredients_meals.ingredient_quantity')
+    errors.add(:base, "Cannot add more than #{meal_max_quantity} #{meal.name} to cart") if quantity > meal_max_quantity
   end
 
   private def set_total
